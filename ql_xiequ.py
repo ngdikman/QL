e5c0736da6bf5ddf6445cc519dbeac4ffc4f1611
ngdikman <diwen.0906@163.com>
2024-05-21 16:09:18 +0800

    Update ql_xiequ.py

--- ql_xiequ.py
+++ ql_xiequ.py
@@ -5,11 +5,12 @@ import requests
 import socket
 
 # 删除所有已有的白名单 IP，uid&ukey改成自己的
-uid = "131626"
-ukey = "3880BE483766E7AE9E85CA7C6DC44DC8"
-delete_url = f"http://op.xiequ.cn/IpWhiteList.aspx?uid={uid}&ukey={ukey}&act=del&ip=all"
-response = requests.get(delete_url)
-print(response.text)
+delete_url1 = "http://op.xiequ.cn/IpWhiteList.aspx?uid=138360&ukey=3306992EFDE6D64642800A28D4C1D939&act=del&ip=all"
+response = requests.get(delete_url1)
+print(response1.text)
+delete_url2 = "http://op.xiequ.cn/IpWhiteList.aspx?uid=131626&ukey=3880BE483766E7AE9E85CA7C6DC44DC8&act=del&ip=all"
+response2 = requests.get(delete_url2)
+print(response2.text)
 
 # 尝试获取宿主机公网IP地址
 ip = ""
@@ -38,7 +39,6 @@ if not ip:
     print(f"无法获取公网IP，使用本地机器IP：{ip}")
 
 # 添加IP到白名单
-add_url = f"http://op.xiequ.cn/IpWhiteList.aspx?uid={uid}&ukey={ukey}&act=add&ip={ip}"
+add_url = f"http://op.xiequ.cn/IpWhiteList.aspx?uid=138360&ukey=3306992EFDE6D64642800A28D4C1D939&act=add&ip={ip}"
 response = requests.get(add_url)
 print(response.text)
-
